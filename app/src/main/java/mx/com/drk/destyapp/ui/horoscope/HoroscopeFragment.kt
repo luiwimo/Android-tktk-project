94a9705527a41b3a4084fb71aa69e908839e6615
Luis <luiwimo@gmail.com>
2024-04-09 08:16:32 -0600

    initial commit

---
+++ app/src/main/java/mx/com/drk/destyapp/ui/horoscope/HoroscopeFragment.kt
@@ -0,0 +1,22 @@
+package mx.com.drk.destyapp.ui.horoscope
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import mx.com.drk.destyapp.databinding.FragmentHoroscopeBinding
+
+class HoroscopeFragment : Fragment() {
+
+    private var _binding: FragmentHoroscopeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHoroscopeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/mx/com/drk/destyapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,22 @@
+package mx.com.drk.destyapp.ui.luck
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import mx.com.drk.destyapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
